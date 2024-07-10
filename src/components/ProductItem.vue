026bf2f2c813b94357f59d8ccb98fb17091ad1e3
Flavia Trotolo <72688128+ft2063@users.noreply.github.com>
2024-07-10 22:54:54 +0400

    front end changes

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -1,31 +1,28 @@
 <template>
     <div class="col-md-4 mb-2">
-        <div class="card">
-            <img :src="product.image" class="card-img-top" alt="" srcset="">
+        <div class="card custom-card text-center">
+            <img :src="product.image" class="card-img-top custom-img" alt="" srcset="">
             <div class="card-body">
                 <h5 class="card-title">{{product.name}}</h5>
-                    <p class="card-text">Lorem ipsum .</p>
-                    <p>
-                        <span class="badge bg-danger">
-                            {{product.price}}
-                        </span>
-
-                    </p>
-                        <button 
-                            @click="data.addToCart(product)"
-                            class="btn btn-primary">
-                            <i class="bi bi-cart-check">
-                                Add to cart
-                            </i>
-                         </button>
-
+                <p>
+                    <span class="badge custom-badge">
+                        ${{product.price}}
+                    </span>
+                </p>
+                <button 
+                    @click="data.addToCart(product)"
+                    class="btn btn-primary">
+                    <i class="bi bi-cart-check">
+                        Add to cart
+                    </i>
+                </button>
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
-    import {useCartStore} from '../stores/useCartStore';
+    import { useCartStore } from '../stores/useCartStore';
 
     const data = useCartStore();
 
@@ -33,12 +30,31 @@
         product: { 
             type: Object,
             required: true
-
         }
-    })
-
+    });
 </script>
 
 <style>
+.custom-card {
+    border-radius: 15px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    transition: transform 0.2s;
+}
+.custom-card:hover {
+    transform: scale(1.01);
+}
 
+.custom-badge {
+    background-color: #28a745;
+    padding: 10px 20px;
+    font-size: 1rem;
+    border-radius: 50px;
+}
+.btn-primary {
+    background-color: #007bff;
+    border: none;
+}
+.btn-primary:hover {
+    background-color: #0056b3;
+}
 </style>
