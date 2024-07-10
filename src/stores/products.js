07f34450a4490476addd09f6db308e206ec491fa
Flavia Trotolo <72688128+ft2063@users.noreply.github.com>
2024-07-10 14:54:49 +0400

    fixing structure and adding pics

---
+++ src/stores/products.js
@@ -0,0 +1,80 @@
+import iphone14 from '../assets/images/iphone-14.png';
+import samsungS20 from '../assets/images/samsung-s20.png';
+import tvLg from '../assets/images/tv-lg.png';
+import oppo14 from '../assets/images/oppo-14.png';
+import macbookPro from '../assets/images/macbook-pro.png';
+import ipadPro from '../assets/images/ipad-pro.png';
+import pixel6 from '../assets/images/pixel-6.png';
+import sonyPlaystation from '../assets/images/sony-playstation.png';
+import xBox from '../assets/images/xBox.png';
+import DJIMini2 from '../assets/images/DJI-mini-2.png';
+import GalaxyWatch4 from '../assets/images/Samsung-Galaxy.png';
+export const products = [
+  {
+    id: 1,
+    name: 'Iphone 14',
+    price: 800,
+    image: iphone14,
+  },
+  {
+    id: 2,
+    name: 'Samsung Galaxy S20',
+    price: 1200,
+    image: samsungS20,
+  },
+  {
+    id: 3,
+    name: 'LG OLED TV',
+    price: 3000,
+    image: tvLg,
+  },
+  {
+    id: 4,
+    name: 'Oppo Find X4',
+    price: 900,
+    image: oppo14,
+  },
+  {
+    id: 5,
+    name: 'MacBook Pro 2023',
+    price: 2500,
+    image: macbookPro,
+  },
+  {
+    id: 6,
+    name: 'iPad Pro 2023',
+    price: 1100,
+    image: ipadPro,
+  },
+  {
+    id: 7,
+    name: 'Google Pixel 6',
+    price: 700,
+    image: pixel6,
+  },
+  {
+    id: 8,
+    name: 'Sony PlayStation 5',
+    price: 500,
+    image: sonyPlaystation,
+  },
+  {
+    id: 9,
+    name: 'Xbox Series X',
+    price: 550,
+    image: xBox, // Replace with actual image URL
+  },
+  {
+    id: 10,
+    name: 'DJI Mavic Air 2',
+    price: 800,
+    image: DJIMini2, // Replace with actual image URL
+  },
+  {
+    id: 11,
+    name: 'Samsung Galaxy Watch 4',
+    price: 300,
+    image: GalaxyWatch4, // Replace with actual image URL
+  },
+  // Add more products as needed
+];
--- src/stores/useCartStore.js
+++ src/stores/useCartStore.js
@@ -1,41 +1,13 @@
 import { defineStore } from "pinia";
 import { useLocalStorage } from "@vueuse/core";
 import { useToast } from "vue-toastification";
-import iphone14 from '../assets/images/iphone-14.png';
-import samsungS20 from '../assets/images/samsung-s20.png';
-import tvLg from '../assets/images/tv-lg.png';
-import oppo14 from '../assets/images/oppo-14.png';
+import { products } from './products.js'; 
 
 const toast = useToast();
 
 export const useCartStore = defineStore('cart', {
   state: () => ({
-    products: [
-      {
-        id: 1,
-        name: 'Iphone 14',
-        price: 800,
-        image: iphone14,
-      },
-      {
-        id: 2,
-        name: 'Samsung s20',
-        price: 1200,
-        image: samsungS20,
-      },
-      {
-        id: 3,
-        name: 'Tv LG',
-        price: 300,
-        image: tvLg,
-      },
-      {
-        id: 4,
-        name: 'Oppo 14',
-        price: 500,
-        image: oppo14,
-      },
-    ],
+    products: products,
     cartItems: useLocalStorage('cartItems', []), // Use local storage for cart items
   }),
   getters: {
